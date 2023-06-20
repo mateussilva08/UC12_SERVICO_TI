30178429fed10240f3e020a9821ce359c026dc86
mateus.rsilva30 <mateus.rsilva30@GUA0636327W10-1>
2023-06-19 21:44:05 -0300

    Adicionar arquivos de projeto.

---
+++ UC12_SERVICO_TI/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace UC12_SERVICO_TI
+{
+    public partial class Form1 : Form
+    {
+        string servidor;
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        public Form1()
+        {
+            InitializeComponent();
+            servidor = "Server=localhost;Database=controle_ti;Uid=root;Pwd=";
+            conexao = new MySqlConnection(servidor);
+            comando = conexao.CreateCommand();
+        }
+
+        private void buttonCADASTRAR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao.Open();
+                comando.CommandText = "INSERT INTO tbl_cliente (nome, cpf, celular, endereco) VALUES ('" + textBoxNOME.Text + "', '" + textBoxCPF.Text + "', '" + textBoxCELULAR.Text + "', '" + textBoxENDERECO.Text + "');";
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+    }
+}
